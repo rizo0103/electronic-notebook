d5888edaa2375d6be4b86f2962202204d86789ed
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2025-11-15 14:53:30 +0500

    users can draw

--- src/components/HandwritingCanvas.jsx
+++ src/components/HandwritingCanvas.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { VscDiscard, VscEdit, VscRedo, VscSymbolField } from 'react-icons/vsc';
 
-const HandwritingCanvas = ({ className }) => {
+const HandwritingCanvas = ({ className, content }) => {
+    const canvasRef = useRef(null);    
+    const [ ctx, setCtx ] = useState(null);
+    const [ isDrawing, setIsDrawing ] = useState(false);
+    const [ penSize, setPenSize ] = useState(5);
+
+    const setupCanvas = () => {
+        const canvas = canvasRef.current,
+              context = canvas.getContext('2d'),
+              rect = canvas.getBoundingClientRect();
+        
+        canvas.width = rect.width;
+        canvas.height = rect.height;
+
+        context.lineCap = 'round';
+        context.lineJoin = 'round';
+        context.lineWidth = penSize;
+        context.globalCompositeOperation = 'source-over';
+
+        setCtx(context);
+    };
+
+    const startDrawing = (e) => {
+        if (!ctx) return;
+        
+        setIsDrawing(true);
     
+        const { x, y } = getPosition(e);
+
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+
+    };
+
+    const draw = (e) => {
+        if (!isDrawing || !ctx) return;
+        
+        const { x, y } = getPosition(e);
+
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    };
+
+    const stopDrawing = () => {
+        if (!ctx) return;
+        setIsDrawing(false);
+
+        ctx.closePath();
+    };
+
+    const getPosition = (e) => {
+        const rect = canvasRef.current.getBoundingClientRect();
+        const clientX = e.touches ? e.touches[0].clientX : e.clientX,
+              clientY = e.touches ? e.touches[0].clientY : e.clientY; 
+
+        return {
+            x: clientX - rect.left,
+            y: clientY - rect.top,
+        }
+    };
+
+    useEffect(() => {
+        setupCanvas();
+    }, []);
+
     return (
         <section className={className}>
             <div className="editor-toolbar">
@@ -11,7 +74,13 @@ const HandwritingCanvas = ({ className }) => {
                 <button className="tool-btn"> <VscDiscard /> </button>
                 <button className="tool-btn"> <VscRedo /> </button>
             </div>
-            <canvas className={className}></canvas>
+            <canvas 
+                className={className}
+                ref={canvasRef}
+                onPointerDown={startDrawing}
+                onPointerMove={draw}
+                onPointerUp={stopDrawing}
+            ></canvas>
         </section>
     )
 };
