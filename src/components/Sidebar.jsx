19bbf0c88fa17f43fdfdd6b1db3186fc045598e0
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2025-11-21 01:57:56 +0500

    now first note is active by default

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,34 @@
 // src/components/Sidebar.jsx
-import React, { useState } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import NoteList from './NoteList';
 import { VscMenu } from 'react-icons/vsc';
+import { getAllNotes } from '../services/noteService';
 
 const Sidebar = ({ notes, activeNoteId, setActiveNoteId, onAddNote, onDeleteNote, onRenameNote, isOpen, onCloseSidebar, onOpenSidebar, sidebarRef, }) => {
     const [ isSidebarOpen, setIsSidebarOpen ] = useState(false);
-    
+    const [ noteId, setNoteId ] = useState(activeNoteId ? activeNoteId : 0);
+
     const handleAddNote = async () => {
         const newNoteId = await onAddNote();
         setActiveNoteId(newNoteId); // Select the new note
     };
 
+    useLayoutEffect(() => {
+        async function getFirstNote() {
+            if (!activeNoteId) {
+                const data = await getAllNotes();
+
+                setNoteId(data[0].id);
+            }
+        }
+
+        getFirstNote();
+    }, []);
+
     if (!isOpen) {
         return <button className='sidebar-toggle' onClick={() => onOpenSidebar()}> <VscMenu /> </button>
     }
+
 
     return (
         <aside ref={sidebarRef} className={`sidebar ${isOpen ? 'active' : ''}`}>
@@ -23,7 +38,7 @@ const Sidebar = ({ notes, activeNoteId, setActiveNoteId, onAddNote, onDeleteNote
             <button className={`add-btn ${isOpen ? 'active' : ''}`} onClick={handleAddNote}>+ New Note</button>
             <NoteList
                 notes={notes}
-                activeNoteId={activeNoteId}
+                activeNoteId={noteId}
                 setActiveNoteId={setActiveNoteId}
                 onDeleteNote={onDeleteNote}
                 onRenameNote={onRenameNote}
